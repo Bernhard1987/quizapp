b2f7cf65aef9d486bc4dc77ad69272e114df7c2b
Bernhard1987 <demoncleaner1@gmail.com>
2023-05-22 21:55:50 +0200

    slight changes...

---
+++ questions.js
@@ -0,0 +1,58 @@
+let questions = [
+{
+    "question": "Wer hat HTML erfunden",
+    "answer_1": "Robbie Williams",
+    "answer_2": "Lady Gaga",
+    "answer_3": "Tim Berners-Lee",
+    "answer_4": "Justin Bieber",
+    "right_answer": 3
+},
+{
+    "question": "Was bedeutet das HTML Tag &lt;a&gt;?",
+    "answer_1": "Hyperlink",
+    "answer_2": "Division",
+    "answer_3": "Image",
+    "answer_4": "Input",
+    "right_answer": 1
+},
+{
+    "question": "Wie definiert man eine Variable in JavaScript?",
+    "answer_1": "numberOne = 20;",
+    "answer_2": "let numberOne = 20;",
+    "answer_3": "20 = let numberOne;",
+    "answer_4": "let 20 = numberOne;",
+    "right_answer": 2
+},
+{
+    "question": "Wer hat HTML erfunden",
+    "answer_1": "Robbie Williams",
+    "answer_2": "Lady Gaga",
+    "answer_3": "Tim Berners-Lee",
+    "answer_4": "Justin Bieber",
+    "right_answer": 3
+},
+{
+    "question": "Wer hat HTML erfunden",
+    "answer_1": "Robbie Williams",
+    "answer_2": "Lady Gaga",
+    "answer_3": "Tim Berners-Lee",
+    "answer_4": "Justin Bieber",
+    "right_answer": 3
+},
+{
+    "question": "Wer hat HTML erfunden",
+    "answer_1": "Robbie Williams",
+    "answer_2": "Lady Gaga",
+    "answer_3": "Tim Berners-Lee",
+    "answer_4": "Justin Bieber",
+    "right_answer": 3
+},
+{
+    "question": "Wer hat HTML erfunden",
+    "answer_1": "Robbie Williams",
+    "answer_2": "Lady Gaga",
+    "answer_3": "Tim Berners-Lee",
+    "answer_4": "Justin Bieber",
+    "right_answer": 3
+},
+];
