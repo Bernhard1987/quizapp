9340e113185a69cd3ca2e9bd5085cb1cd2b9e621
Bernhard1987 <demoncleaner1@gmail.com>
2023-06-01 10:39:49 +0200

    added start screen

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 let currentQuestion = 0;
 let rightAnswerCount = 0;
-
-function init() {
-    document.getElementById('totalQuestionCount').innerHTML = questions.length;
-    showQuestion();
-}
+let AUDIO_SUCCESS = new Audio('sounds/ríght.mp3');
+let AUDIO_FAIL = new Audio('sounds/wrong.mp3');
 
 function showQuestion() {
     let question = questions[currentQuestion];
+    showQuestionHTML();
+    document.getElementById('totalQuestionCount').innerHTML = questions.length;
+    updateProgressbar();
+    updateQuestion(question);
+}
+
+function updateProgressbar() {
     let progressbar = document.getElementById('progressbar');
     let currentPercentage = 100 / questions.length * currentQuestion;
     progressbar.ariaValueNow = currentPercentage;
     progressbar.style = `width: ${currentPercentage}%`;
     progressbar.innerHTML = `${currentPercentage.toFixed(0)}%`;
+}
+
+function updateQuestion(question) {
     document.getElementById('currentQuestion').innerHTML = currentQuestion+1;
     document.getElementById('nextQuestionBtn').disabled = true;
     document.getElementById('questionText').innerHTML = question['question'];
@@ -26,41 +33,116 @@ function answer(selectedAnswer) {
     let question = questions[currentQuestion];
     let selectedQuestionNumber = selectedAnswer.slice(-1);
     let idOfRightAnswer = `answer_${question['right_answer']}`;
-
-    if (question['right_answer'] == selectedQuestionNumber) {
-        document.getElementById(selectedAnswer).parentNode.classList.add('correctanswer');
-        document.getElementById('letter_'+selectedAnswer).classList.add('correctanswer-letter');
-        rightAnswerCount++;
-    } else if (question['right_answer'] != selectedQuestionNumber) {
-        document.getElementById(selectedAnswer).parentNode.classList.add('wronganswer');
-        document.getElementById('letter_'+selectedAnswer).classList.add('wronganswer-letter');
-        document.getElementById(idOfRightAnswer).parentNode.classList.add('correctanswer');
-        document.getElementById('letter_'+idOfRightAnswer).classList.add('correctanswer-letter');
+    if (rightAnswerSelected(question, selectedQuestionNumber)) {
+        correctAnswerAction(selectedAnswer);
+    } else {
+        wrongAnswerAction(selectedAnswer, idOfRightAnswer);
     }
     document.getElementById('nextQuestionBtn').disabled = false;
 }
 
+function rightAnswerSelected(question, selectedQuestionNumber) {
+    return question['right_answer'] == selectedQuestionNumber;
+}
+
+function correctAnswerAction(selectedAnswer) {
+    document.getElementById(selectedAnswer).parentNode.classList.add('correctanswer');
+    document.getElementById('letter_'+selectedAnswer).classList.add('correctanswer-letter');
+    AUDIO_SUCCESS.play();
+    rightAnswerCount++;
+}
+
+function wrongAnswerAction(selectedAnswer, idOfRightAnswer) {
+    document.getElementById(selectedAnswer).parentNode.classList.add('wronganswer');
+    document.getElementById('letter_'+selectedAnswer).classList.add('wronganswer-letter');
+    document.getElementById(idOfRightAnswer).parentNode.classList.add('correctanswer');
+    document.getElementById('letter_'+idOfRightAnswer).classList.add('correctanswer-letter');
+    AUDIO_FAIL.play();
+}
+
 function nextQuestion() {
-    if (currentQuestion+1 == questions.length) {
-        document.getElementById('quizCard').innerHTML = '';
-        document.getElementById('quizCard').innerHTML += `
-        <div class="finalScreen">
-            <div class="trophy">
-                <img src="./img/tropy.png">
-                <img src="./img/tropy.png" class="imgMirrorY">
-            </div>
-            <h2>Herzlichen Glückwunsch!</h2><br>
-            <h3>Du hast ${rightAnswerCount} Fragen von ${questions.length} Fragen richtig beantwortet!</h3>
-            <button class="btn buttonbg" onclick="location.reload()">nochmal spielen!</button>
-        </div>
-        `;
-        document.getElementById('progressbar').style = 'width: 100%';
-        document.getElementById('progressbar').innerHTML = '100%';
+    if (questionNumberEqualsLength()) {
+        showEndscreen();
     } else {
-        currentQuestion++;
-        showQuestion();
-        removeClass();
+        showNextQuestion();
     }
+}
+
+function questionNumberEqualsLength() {
+    return currentQuestion+1 == questions.length;
+}
+
+function showStartScreenHTML() {
+    document.getElementById('rightSection').innerHTML = '';
+    document.getElementById('rightSection').innerHTML = `
+    <div class="startScreen">
+          <div class="startScreenContent">
+            <h1>Willkommen beim beeindruckenden HTML Quiz!</h1>
+            <div>Bereit für die Herausforderung?</div>
+          </div>
+          <div class="startScreenBottom">
+            <button class="btn startScreenButton" onclick="showQuestion()">JETZT STARTEN!<img src="./img/arrow_forward.png" alt="" class="arrow"></button>
+          </div>
+        </div>
+    `;
+}
+
+function showQuestionHTML() {
+    document.getElementById('rightSection').innerHTML = '';
+    document.getElementById('rightSection').innerHTML += `
+    <div class="card-body">
+    <h5 class="card-title" id="questionText"></h5>
+    <div class="card mb-2 quiz-answer-card dflexrow" onclick="answer('answer_1')">
+      <div class="answerLetter" id="letter_answer_1">A</div>
+      <div class="card-body" id="answer_1">
+      </div>
+    </div>
+    <div class="card mb-2 quiz-answer-card dflexrow" onclick="answer('answer_2')">
+      <div class="answerLetter" id="letter_answer_2">B</div>
+      <div class="card-body" id="answer_2">
+      </div>
+    </div>
+    <div class="card mb-2 quiz-answer-card dflexrow" onclick="answer('answer_3')">
+      <div class="answerLetter" id="letter_answer_3">C</div>
+      <div class="card-body" id="answer_3">
+      </div>
+    </div>
+    <div class="card mb-2 quiz-answer-card dflexrow" onclick="answer('answer_4')">
+      <div class="answerLetter" id="letter_answer_4">D</div>
+      <div class="card-body" id="answer_4">
+      </div>
+    </div>
+    <div class="question-footer">
+      <div>
+        <b id="currentQuestion">1</b> von <b id="totalQuestionCount">5</b> Fragen
+      </div>
+      <button class="btn circlebutton" id="nextQuestionBtn" onclick="nextQuestion()"></button>
+    </div>
+  </div>
+  `;
+}
+
+function showEndscreen() {
+    document.getElementById('quizCard').innerHTML = '';
+    document.getElementById('quizCard').innerHTML += `
+    <div class="finalScreen">
+        <div class="trophy">
+            <img src="./img/tropy.png">
+            <img src="./img/tropy.png" class="imgMirrorY">
+        </div>
+        <h2>Herzlichen Glückwunsch!</h2><br>
+        <h3>Du hast ${rightAnswerCount} Fragen von ${questions.length} Fragen richtig beantwortet!</h3>
+        <button class="btn buttonbg" onclick="location.reload()">nochmal spielen!</button>
+    </div>
+    `;
+    document.getElementById('progressbar').style = 'width: 100%';
+    document.getElementById('progressbar').innerHTML = '100%';
+}
+
+function showNextQuestion() {
+    currentQuestion++;
+    showQuestion();
+    removeClass();
 }
 
 function removeClass() {
